c28dbe7f9bd51c327e6f918f171ea823e42d963a
Peter Shen <peter.shen@aimsio.com>
2018-05-01 00:35:05 -0400

    remove cores

--- run_model.sh
+++ run_model.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p short   # queue name
 #SBATCH -t 0-12:00       # hours:minutes runlimit after which job will be killed.
-#SBATCH -n 32      # number of cores requested
+#SBATCH -n 20      # number of cores requested
 #SBATCH --mem=32G # memory requested
 #SBATCH -J 707model         # Job name
 #SBATCH -o %j.out       # File to which standard out will be written
